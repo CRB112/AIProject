f93f6740bdafc3a859479b9560f16fa8ce3ebfc9
Connor <connorrb1111@gmail.com>
2026-03-23 14:57:53 -0400

    Added comments to hiddenMarkov assignment

--- algorithms/hiddenMarkov/hiddenMarkov.py
+++ algorithms/hiddenMarkov/hiddenMarkov.py
@@ -3,7 +3,8 @@ import random
 
 maxObservations = 10
 
-#Returns an observation sequence []
+#Returns an observation sequence [] based on start start and
+#Emission probabilities
 def getObsSeq(startState, statesD, maxO):
     states = list(statesD.keys())
     seq = []
@@ -16,7 +17,6 @@ def getObsSeq(startState, statesD, maxO):
     return seq
 
 def algo(startState, statesD):
-
     obs = getObsSeq(startState, statesD, maxObservations)
     states = list(statesD.keys())
     numObs = len(obs)
@@ -24,7 +24,7 @@ def algo(startState, statesD):
     V = [{} for _ in range(numObs)] # Setting up blank table
     path = {} # settings up blank path
     
-
+    #Settings first state to 'startState'
     for state in states:
         if state == startState:
             V[0][state] = 1
@@ -33,6 +33,13 @@ def algo(startState, statesD):
         path[state] = [state]
 
     print("Start state is " + str(startState) + "\n")
+    #Main algorithm
+    #takes steps starting at 1 (startState already established)
+    #creates a new path
+    #Checks each state and saves the probabililty of reaching the current state
+    #Multiplies transition probability x emission probability x previous vertibi probability
+    #If the probability of traversing through all of these states to the current has the highest
+    #Probability values, selects the previous state that maximmizes probability and extends the path
     for step in range(1, numObs):
         newPath = {}
 
@@ -50,8 +57,7 @@ def algo(startState, statesD):
             V[step][curr] = maxProb
             newPath[curr] = path[prevStateSel] + [curr]
 
-
-
+        #New path setting for next iteration and prints some data
         path = newPath
         bestCurr = max(V[step], key=V[step].get)
         print(f"Step {step}, Observation: {obs[step]}")
@@ -59,6 +65,8 @@ def algo(startState, statesD):
         print(f"Current best path: {path[bestCurr]}")
         print("======Next Step======\n")
 
+    #The algorithm has reached the last step and grabs the emission probability
+    #From the previous step to the finish
     maxProb = -1
     lastState = None
     for state in states:
